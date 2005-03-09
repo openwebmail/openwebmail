d5f1a325c61d9a045103d719c2db5ca347985b74
Alex Teslik <alex@acatysmoof.com>
2005-03-09 12:00:00 +0000

    1. support using either SentDate and Received Date as message timestamp 2. messagelist code clean 3. calculate message age based on its received date so messages with long    transmission delay won't be removed soon by clean_trash_spamvirus() 4. show transmission delay as the tips of messagelist date field 5. finalize move of all en.utf8 symlinks to real directories
    git-svn-id: svn://openwebmail.acatysmoof.com/svn/trunk@209 9f51437f-89f6-0310-a5ff-ff9a3806d2ee

---
+++ src/data/openwebmail/javascript/htmlarea.openwebmail/popups/en.utf8/htmlarea-lang.js
@@ -0,0 +1,84 @@
+// I18N constants
+
+// Translation Note
+// 1. translate the left  side of fontname, font size and formatblock
+// 2. translate the right side of tooltips
+
+HTMLArea.I18N = {
+	fontname: {
+		"Arial":           'arial,helvetica,sans-serif',
+		"Courier New":     'courier new,courier,monospace',
+		"Georgia":         'georgia,times new roman,times,serif',
+		"Tahoma":          'tahoma,arial,helvetica,sans-serif',
+		"Times New Roman": 'times new roman,times,serif',
+		"Verdana":         'verdana,arial,helvetica,sans-serif',
+		"impact":          'impact',
+		"WingDings":       'wingdings'
+	},
+
+	fontsize: {
+		"1 (8 pt)":  "1",
+		"2 (10 pt)": "2",
+		"3 (12 pt)": "3",
+		"4 (14 pt)": "4",
+		"5 (18 pt)": "5",
+		"6 (24 pt)": "6",
+		"7 (36 pt)": "7"
+	},
+
+	formatblock: {
+		"Heading 1": "h1",
+		"Heading 2": "h2",
+		"Heading 3": "h3",
+		"Heading 4": "h4",
+		"Heading 5": "h5",
+		"Heading 6": "h6",
+		"Normal": "p",
+		"Address": "address",
+		"Formatted": "pre"
+	},
+
+	tooltips: {
+		bold:           "Bold",
+		italic:         "Italic",
+		underline:      "Underline",
+		strikethrough:  "Strikethrough",
+		subscript:      "Subscript",
+		superscript:    "Superscript",
+		justifyleft:    "Justify Left",
+		justifycenter:  "Justify Center",
+		justifyright:   "Justify Right",
+		justifyfull:    "Justify Full",
+		insertorderedlist:    "Ordered List",
+		insertunorderedlist:  "Bulleted List",
+		outdent:        "Decrease Indent",
+		indent:         "Increase Indent",
+		forecolor:      "Font Color",
+		hilitecolor:    "Background Color",
+		inserthorizontalrule: "Horizontal Rule",
+		createlink:     "Insert Web Link",
+		insertimage:    "Insert Image",
+		insertsound:    "Insert Sound",
+		inserttable:    "Insert Table",
+		htmlmode:       "Toggle HTML Source",
+		popupeditor:    "Enlarge Editor",
+		about:          "About this editor",
+		showhelp:       "Help using editor",
+		textindicator:  "Current style",
+		undo:           "Undoes your last action",
+		redo:           "Redoes your last action",
+		cut:            "Cut selection",
+		copy:           "Copy selection",
+		paste:          "Paste from clipboard"
+	},
+
+	buttons: {
+		"ok":           "OK",
+		"cancel":       "Cancel"
+	},
+
+	msg: {
+		"Path":         "Path",
+		"TEXT_MODE":    "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWIG."
+	}
+};
