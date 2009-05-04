942ce6966960d47a356d74976bffdabfe3615335
Alex Teslik <alex@acatysmoof.com>
2009-05-04 03:55:02 +0000

    fix bug 24 that cal events could not be edited from the dayview form
    simplify dereferencing syntax and general code cleanup on htmlrender.pl
    
    
    
    git-svn-id: svn://openwebmail.acatysmoof.com/svn/trunk@356 9f51437f-89f6-0310-a5ff-ff9a3806d2ee

--- src/cgi-bin/openwebmail/modules/htmlrender.pl
+++ src/cgi-bin/openwebmail/modules/htmlrender.pl
@@ -48,9 +48,9 @@ my @jsevents = (
                  'document.location', 'document.referrer'
                );
 
-# since this routine deals with the base directive it must be called
-# first before any other routines in this module when converting html
 sub html4nobase {
+   # since this routine deals with the base directive it must be called
+   # first before any other routines in this module when converting html
    my $html = shift;
 
    my $urlbase = '';
@@ -61,38 +61,41 @@ sub html4nobase {
    }
 
    $html =~ s#<base\s+([^<>]*?)>##gi;
+
    if ($urlbase ne '' && $urlbase !~ m/^file:/i) {
       $html =~ s#(<a\s+href|background|src|method|action)(=\s*"?)#$1$2$urlbase#gi;
       # recover links that should not be changed by base directive
       $html =~ s#\Q$urlbase\E(http://|https://|ftp://|mms://|cid:|mailto:|\#)#$1#gi;
    }
-   return($html);
+
+   return $html;
 }
 
-# this routine is used to add target=_blank to links in a html message
-# so clicking on it will open a new window
 sub html4link {
+   # this routine is used to add target=_blank to links in a html message
+   # so clicking on it will open a new window
    my $html = shift;
    $html =~ s#(<a\s+[^<>]*?>)#_link_target_blank($1)#igems;
-   return($html);
+   return $html;
 }
 
 sub _link_target_blank {
    my $link = shift;
 
-   return($link) if ($link =~ m/(?:target=|javascript:|href="?#)/i);
+   return $link if $link =~ m/(?:target=|javascript:|href="?#)/i;
 
    $link =~ s#<a\s+([^<>]*?)>#<a $1 target="_blank">#is;
-   return($link);
+
+   return $link;
 }
 
-# this routine is used to resolve framesets in html by
-# converting <frame ...> into <iframe width="100%"..></iframe>
-# so html with framesets can be displayed correctly inside the message body
 sub html4noframe {
+   # this routine is used to resolve framesets in html by
+   # converting <frame ...> into <iframe width="100%"..></iframe>
+   # so html with framesets can be displayed correctly inside the message body
    my $html = shift;
    $html =~ s#(<frame\s+[^<>]*?>)#_frame2iframe($1)#igems;
-   return($html);
+   return $html;
 }
 
 sub _frame2iframe {
@@ -100,12 +103,12 @@ sub _frame2iframe {
    return '' if ($frame !~ m#src=#i);
    $frame =~ s#<frame #<iframe width="100%" height="250" #is;
    $frame .= '</iframe>';
-   return($frame);
+   return $frame;
 }
 
-# this routine disables the javascript in an html message
-# to avoid the user being hijacked by some evil programs
 sub html4disablejs {
+   # this routine disables the javascript in an html message
+   # to avoid the user being hijacked by some evil programs
    my $html = shift;
 
    foreach my $event (@jsevents) {
@@ -121,12 +124,12 @@ sub html4disablejs {
    # disable inline javascript
    $html =~ s#<([^<]*?[='"\s]+)javascript:#<$1disable_javascript:#isg;
 
-   return($html);
+   return $html;
 }
 
-# this routine disables embed, applet, object tags in an html message
-# to avoid user being hijacked by some evil programs
 sub html4disableembcode {
+   # this routine disables embed, applet, object tags in an html message
+   # to avoid user being hijacked by some evil programs
    my $html = shift;
    foreach my $tag (qw(embed applet object)) {
       $html =~ s#<\s*$tag([^<>]*?)>#<disable_$tag$1>#isg;
@@ -136,9 +139,9 @@ sub html4disableembcode {
    return $html;
 }
 
-# this routine disables the embedded CGI in a html message
-# to avoid user email addresses being confirmed by spammer through embedded CGIs
 sub html4disableemblink {
+   # this routine disables the embedded CGI in a html message
+   # to avoid user email addresses being confirmed by spammer through embedded CGIs
    my ($html, $disableemblink, $blankimgurl) = @_;
    return $html if !defined $disableemblink || $disableemblink eq 'none';
    $html =~ s#(src|background)\s*=\s*("?https?://[\w\.\-]+?/?[^\s<>]*)([\b|\n| ]*)#_clean_emblink($1,$2,$3,$disableemblink,$blankimgurl)#egis;
@@ -147,7 +150,7 @@ sub html4disableemblink {
 
 sub _clean_emblink {
    my ($type, $url, $end, $disableemblink, $blankimgurl) = @_;
-   if ($url !~ /\Q$ENV{'HTTP_HOST'}\E/is) { # non-local URL found
+   if ($url !~ /\Q$ENV{HTTP_HOST}\E/is) { # non-local URL found
       if ($disableemblink eq 'cgionly' && $url =~ m/\?/s) {
          $url =~ s/["']//g;
          return(qq|$type="$blankimgurl" border="1" alt="Embedded CGI removed by OpenWebMail: $url" onclick="window.open('$url', '_extobj');"$end|);
@@ -159,17 +162,17 @@ sub _clean_emblink {
    return("$type=$url".$end);
 }
 
-# this routine is used to resolve cid or loc in an html message to
-# the cgi openwebmail-viewatt.pl links of cross referenced mime objects
-# this is for read message
 sub html4attachments {
+   # this routine is used to resolve cid or loc in an html message to
+   # the cgi openwebmail-viewatt.pl links of cross referenced mime objects
+   # this is for read message
    my ($html, $r_attachments, $scripturl, $scriptparm) = @_;
 
-   for (my $i=0; $i<=$#{$r_attachments}; $i++) {
-      my $filename = ow::tool::escapeURL(${${$r_attachments}[$i]}{filename});
-      my $link = qq|$scripturl/$filename?$scriptparm&amp;attachment_nodeid=${${$r_attachments}[$i]}{nodeid}&amp;|;
-      my $cid = ${${$r_attachments}[$i]}{'content-id'} || '';
-      my $loc = ${${$r_attachments}[$i]}{'content-location'} || '';
+   for (my $i = 0; $i <= $#{$r_attachments}; $i++) {
+      my $filename = ow::tool::escapeURL($r_attachments->[$i]{filename});
+      my $link = qq|$scripturl/$filename?$scriptparm&amp;attachment_nodeid=$r_attachments->[$i]{nodeid}&amp;|;
+      my $cid = $r_attachments->[$i]{'content-id'} || '';
+      my $loc = $r_attachments->[$i]{'content-location'} || '';
 
       if ( ($cid ne '' && $html =~ s#(?:cid:)+\Q$cid\E#$link#sig ) ||
            ($loc ne '' && $html =~ s#(?:cid:)*\Q$loc\E#$link#sig ) ||
@@ -178,98 +181,106 @@ sub html4attachments {
                $html =~ s#(background|src)\s*=\s*"[^\s\<\>"]{0,256}?/$filename"#$1="$link"#sig) )
          ) {
          # this attachment is referenced by the html
-         ${${$r_attachments}[$i]}{referencecount}++;
+         $r_attachments->[$i]{referencecount}++;
       }
    }
-   return($html);
+   return $html;
 }
 
-# this routine is used to resolve cid or loc in a html message to
-# the cgi openwebmail-viewatt.pl links of cross referenced mime objects
-# this is for message composing
 sub html4attfiles {
+   # this routine is used to resolve cid or loc in a html message to
+   # the cgi openwebmail-viewatt.pl links of cross referenced mime objects
+   # this is for message composing
    my ($html, $r_attfiles, $scripturl, $scriptparm)=@_;
 
-   for (my $i=0; $i<=$#{$r_attfiles}; $i++) {
-      my $filename=ow::tool::escapeURL(${${$r_attfiles}[$i]}{name});
-      my $link=qq|$scripturl/$filename?$scriptparm&amp;|.
-               qq|attfile=|.ow::tool::escapeURL(${${$r_attfiles}[$i]}{file}).qq|&amp;|;
-      my $cid="cid:"."${${$r_attfiles}[$i]}{'content-id'}";
-      my $loc=${${$r_attfiles}[$i]}{'content-location'};
+   for (my $i = 0; $i <= $#{$r_attfiles}; $i++) {
+      my $filename = ow::tool::escapeURL($r_attfiles->[$i]{name});
+      my $link = qq|$scripturl/$filename?$scriptparm&amp;| .
+                 qq|attfile=| . ow::tool::escapeURL($r_attfiles->[$i]{file}) . qq|&amp;|;
+      my $cid = "cid:$r_attfiles->[$i]{'content-id'}";
+      my $loc = $r_attfiles->[$i]{'content-location'};
 
-      if ( ($cid ne "cid:" && $html =~ s#\Q$cid\E#$link#sig ) ||
-           ($loc ne "" && $html =~ s#\Q$loc\E#$link#sig ) ||
-           ($filename ne "" && 			# ugly hack for strange CID
-              ($html =~ s#CID:\{[\d\w\-]+\}/$filename#$link#sig ||
-               $html =~ s#(background|src)\s*=\s*"[^\s\<\>"]{0,256}?/$filename"#$1="$link"#sig) )
+      if (
+           ($cid ne "cid:" && $html =~ s#\Q$cid\E#$link#sig )
+           || ($loc ne "" && $html =~ s#\Q$loc\E#$link#sig )
+           || ($filename ne ""
+               && ( # ugly hack for strange CID
+                    $html =~ s#cid:\{[\d\w\-]+\}/$filename#$link#sig
+                    || $html =~ s#(background|src)\s*=\s*"[^\s\<\>"]{0,256}?/$filename"#$1="$link"#sig
+                  )
+              )
          ) {
          # this attachment is referenced by the html
-         ${${$r_attfiles}[$i]}{referencecount}++;
+         $r_attfiles->[$i]{referencecount}++;
       }
    }
-   return($html);
+   return $html;
 }
 
-# this routine is used to revert links of crossreferenced mime objects
-# back to their cid or loc equivelents. This is the reverse operation
-# of html4attfiles() and is used for message sending
 sub html4attfiles_link2cid {
+   # this routine is used to revert links of crossreferenced mime objects
+   # back to their cid or loc equivelents. This is the reverse operation
+   # of html4attfiles() and is used for message sending
    my ($html, $r_attfiles, $scripturl)=@_;
-   $html=~s!(src|background|href)\s*=\s*("?https?://[\w\.\-]+?/?[^\s<>]*[\w/"])([\b|\n| ]*)!_link2cid($1,$2,$3, $r_attfiles, $scripturl)!egis;
+   $html =~ s#(src|background|href)\s*=\s*("?https?://[\w\.\-]+?/?[^\s<>]*[\w/"])([\b|\n| ]*)#_link2cid($1,$2,$3, $r_attfiles, $scripturl)#egis;
    return($html);
 }
 
 sub _link2cid {
    my ($type, $url, $end, $r_attfiles, $scripturl)=@_;
-   for (my $i=0; $i<=$#{$r_attfiles}; $i++) {
-      my $filename=ow::tool::escapeURL(${${$r_attfiles}[$i]}{name});
-      my $attfileparm=qq|attfile=|.ow::tool::escapeURL(${${$r_attfiles}[$i]}{file});
-      if ($url=~ /\Q$scripturl\E/ && $url=~ /\Q$attfileparm\E/) {
-         ${${$r_attfiles}[$i]}{referencecount}++;
+   for (my $i = 0; $i <= $#{$r_attfiles}; $i++) {
+      my $filename    = ow::tool::escapeURL($r_attfiles->[$i]{name});
+      my $attfileparm = 'attfile=' . ow::tool::escapeURL($r_attfiles->[$i]{file});
+      if ($url =~ m/\Q$scripturl\E/ && $url =~ m/\Q$attfileparm\E/) {
+         $r_attfiles->[$i]{referencecount}++;
 
-         my $cid="cid:${${$r_attfiles}[$i]}{'content-id'}";
-         my $loc=${${$r_attfiles}[$i]}{'content-location'};
-         return(qq|$type="$cid"$end|) if ($cid ne "cid:");
-         return(qq|$type="$loc"$end|) if ($loc);
+         my $cid = "cid:$r_attfiles->[$i]{'content-id'}";
+         my $loc = $r_attfiles->[$i]{'content-location'};
+         return qq|$type="$cid"$end| if $cid ne 'cid:';
+         return qq|$type="$loc"$end| if defined $loc && $loc;
          # construct strange CID from attserial
-         ${${$r_attfiles}[$i]}{file}=~/([\w\d\-]+)$/;
-         return(qq|$type="CID:{$1}/$filename"$end|) if ($filename);
+         $r_attfiles->[$i]{file} =~ m/([\w\d\-]+)$/;
+         return qq|$type="cid:{$1}/$filename"$end| if $filename;
       }
    }
-   return("$type=$url".$end);
+   return ("$type=$url".$end);
 }
 
-# this routine changes mailto: links into native composemessage links
-# so that clicking a mailto will launch an internal composemessage session
-# to work with the base directive we use full urls
-# to keep compatibility with undecoded base64 blocks we create the new url
-# as a separated line
 sub html4mailto {
+   # this routine changes mailto: links into native composemessage links
+   # so that clicking a mailto will launch an internal composemessage session
+   # to work with the base directive we use full urls
+   # to keep compatibility with undecoded base64 blocks we create the new url
+   # as a separated line
    my ($html, $ow_sendurl) = @_;
+
    $html =~ s/(=\s*"?)mailto:\s*([^"'>]*?)(["'>])/_mailtoparm($1,$2,$ow_sendurl,$3)/egis;
-   return($html);
+
+   return $html;
 }
 
-# convert standard mailto links into internal composemessage links
 sub _mailtoparm {
+   # convert standard mailto links into internal composemessage links
    my ($mailtoprefix, $mailtourl, $ow_sendurl, $mailtosuffix) = @_;
+
    $mailtourl = '' unless defined $mailtourl;
    $mailtourl =~ s#&amp;#&#g;
    $mailtourl =~ s#\?#&#;
    $mailtourl =~ s#&#&amp;#g;
+
    return $mailtoprefix . $ow_sendurl . "&amp;to=" . ow::tool::escapeURL($mailtourl) . $mailtosuffix;
 }
 
-# remove body tags and encapsulate html in a table for message reading
 sub html2table {
+   # remove body tags and encapsulate html in a table for message reading
    my $html = _htmlclean(shift);
    $html =~ s#<body([^<>]*?)>#<table cellpadding="2" cellspacing="0" border="0" width="100%" $1><tr><td>#is;
    $html =~ s#</body>#</td></tr></table>#is;
    return $html;
 }
 
-# remove html body tags for message composing
 sub html2block {
+   # remove html body tags for message composing
    my $html = _htmlclean(shift);
    $html =~ s#<body([^<>]*?)>##is;
    $html =~ s#</body>##is;
@@ -292,7 +303,8 @@ sub _htmlclean {
    $html =~ s#<[^<>]*?stylesheet[^<>]*?>##gis;
    $html =~ s#(<div[^<>]*?)position\s*:\s*absolute\s*;([^<>]*?>)#$1$2#gis;
    $html =~ s#(style\s*=\s*"?[^"'>]*?)position\s*:\s*absolute\s*;([^"'>]*?"?)#$1$2#gis;
-   return($html);
+
+   return $html;
 }
 
 1;
