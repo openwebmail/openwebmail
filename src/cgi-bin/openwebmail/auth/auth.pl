7f0748030a0f761c39859b9cfb909c7540498eef
Alex Teslik <alex@acatysmoof.com>
2010-11-07 07:30:24 +0000

    move the loadmodule subroutine out of modules/tool and into auth since this is the only place it is used anywhere
    NOTE: this revision does not build or function correctly.
    This notice will be removed when the revision is functional.
    
    
    git-svn-id: svn://openwebmail.acatysmoof.com/svn/trunk@507 9f51437f-89f6-0310-a5ff-ff9a3806d2ee

--- src/cgi-bin/openwebmail/auth/auth.pl
+++ src/cgi-bin/openwebmail/auth/auth.pl
@@ -10,12 +10,41 @@ require "modules/tool.pl";
 sub load {
    my $authfile=$_[0];
    my $ow_cgidir=$INC[$#INC];	# get cgi-bin/openwebmail path from @INC
-   ow::tool::loadmodule("ow::auth::internal",
-                        "$ow_cgidir/auth", $authfile,
-                        "get_userinfo",
-                        "get_userlist",
-                        "check_userpassword",
-                        "change_userpassword");
+   loadmodule("ow::auth::internal",
+              "$ow_cgidir/auth", $authfile,
+              "get_userinfo",
+              "get_userlist",
+              "check_userpassword",
+              "change_userpassword");
+}
+
+# use 'require' to load the package ow::$file
+# then alias ow::$file::symbol to $newpkg::symbol
+# through Glob and 'tricky' symbolic reference feature
+sub loadmodule {
+   my ($newpkg, $moduledir, $modulefile, @symlist)=@_;
+   $modulefile=~s|/||g; $modulefile=~s|\.\.||g; # remove / and .. for path safety
+
+   # this would be done only once because of %INC
+   my $modulepath=ow::tool::untaint("$moduledir/$modulefile");
+   require $modulepath;
+
+   # . - is not allowed for package name
+   my $modulepkg='ow::'.$modulefile;
+   $modulepkg=~s/\.pl//;
+   $modulepkg=~s/[\.\-]/_/g;
+
+   # release strict refs until block end
+   no strict 'refs';
+   # use symbol table of package $modulepkg if no symbol passed in
+   @symlist=keys %{$modulepkg.'::'} if ($#symlist<0);
+
+   foreach my $sym (@symlist) {
+      # alias symbol of sub routine into current package
+      *{$newpkg.'::'.$sym}=*{$modulepkg.'::'.$sym};
+   }
+
+   return;
 }
 
 sub get_userlist {
