62edd34d99afb950dd9567aa92941f823aa7c8c7
Alex Teslik <alex@acatysmoof.com>
2013-11-15 14:02:14 -0800

    reorganize master to facilitate building and working with git

--- openwebmail.acatysmoof.com/archive/tools/post_lib/BBCode/Tag/ACRONYM.pm
+++
@@ -1,8 +0,0 @@
-# $Id: ACRONYM.pm 79 2005-08-26 04:04:10Z chronos $
-package BBCode::Tag::ACRONYM;
-use base qw(BBCode::Tag::ABBR);
-use strict;
-use warnings;
-our $VERSION = '0.01';
-
-1;
--- openwebmail.acatysmoof.com/archive/tools/post_lib/BBCode/Tag/UL.pm
+++
@@ -1,8 +0,0 @@
-# $Id: UL.pm 79 2005-08-26 04:04:10Z chronos $
-package BBCode::Tag::UL;
-use base qw(BBCode::Tag::LIST);
-use strict;
-use warnings;
-our $VERSION = '0.01';
-
-1;
